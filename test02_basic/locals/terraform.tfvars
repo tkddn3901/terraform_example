c3c876796d2d19904f5a0ecb4bd58040d0d70443
tkddn3901 <tkddn3901@gmail.com>
2026-04-30 09:32:57 +0900

    local example added

---
+++ test02_basic/locals/terraform.tfvars
@@ -0,0 +1,8 @@
+# locals/terraform.tfvars
+
+# 선언되어 있는 변수에 넣을 값을 여기서 결정할 수 있다.
+# terraform.tfvars 라는 파일의 이름은 정해진 이름이다.
+# terraform.tfvars 는 default 설정이다.
+
+env = "dev"
+project_name = "ktcloud"
---
+++ test02_basic/locals/variables.tf
@@ -0,0 +1,11 @@
+# locals/variables.tf
+
+variable "env" {
+    type = string
+    description = "현재 환경(dev|prod)"
+}
+
+variable "project_name" {
+    type = string
+    description = "프로젝트 이름"
+}
